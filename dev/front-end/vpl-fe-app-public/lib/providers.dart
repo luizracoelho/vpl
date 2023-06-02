f4d16e282bf536cf2a94983aad6a285b330eeecd
lucasbalani <lucas.balani@valorup.com.br>
2023-06-02 00:33:52 -0300

    Criado listagem por modelo na tela de veículos.

--- dev/front-end/vpl-fe-app-public/lib/providers.dart
+++ dev/front-end/vpl-fe-app-public/lib/providers.dart
@@ -7,12 +7,12 @@ import 'features/brands/states/brands_list_state.dart';
 import 'features/models/states/models_list_state.dart';
 import 'features/shared/states/primary_flow_state.dart';
 import 'features/shared/states/theme_state.dart';
-import 'features/vehicles/states/vehicles_list_state.dart';
+import 'features/vehicles/states/vehicle_list_state.dart';
 
 final List<SingleChildWidget> providers = [
   ChangeNotifierProvider(create: (_) => ThemeState(Brightness.dark)),
   ChangeNotifierProvider(create: (_) => PrimaryFlowState()),
   ChangeNotifierProvider(create: (_) => BrandsListState()),
   ChangeNotifierProvider(create: (_) => ModelsListState()),
-  ChangeNotifierProvider(create: (_) => VehiclesListState()),
+  ChangeNotifierProvider(create: (_) => VehicleListState()),
 ];
